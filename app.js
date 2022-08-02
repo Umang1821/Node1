771fc05aa02b4de24c6c1e398b8c5c7c87a6066f
Umang1821 <77398534+Umang1821@users.noreply.github.com>
2022-08-02 17:28:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+
+const dbURI = 'mongodb+srv://Umang:Gemini123@@clustermongo.pf5cky0.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
